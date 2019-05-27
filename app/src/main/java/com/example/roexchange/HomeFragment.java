76b04b2ee38d4f5446745a2da03f76e35778c9f2
Felix Andrian <felixlix45@gmail.com>
2019-05-27 10:43:08 +0700

    Add Fragment to each page

---
+++ app/src/main/java/com/example/roexchange/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.example.roexchange;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.roexchange.R;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+    }
+}
